f98f70f92437ba4c8453b11d32c2a54ae1956cfb
Devon Heaston <dheaston2@huskers.unl.edu>
2026-08-27 10:56:24 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Jackson Payne!\n");
   printf("Computer Science\n");
+  printf("Devon Heaston!\n");
+  printf("Computer Engineering\n");
 
   return 0;
 }
