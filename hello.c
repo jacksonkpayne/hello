530b4a3f93450c2c9d451fc07fdae98571dc0882
jacksonkpayne <jacksonkpayne@gmail.com>
2026-08-27 09:40:34 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/*
+    Author: Jackson Payne
+    Date: 8/27/2026
+
+    Prints my name
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jackson Payne\n");
+
+  return 0;
+}
